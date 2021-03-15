dfeb850e497b7fa4297324f93333c46219cdfc8a
Simen A. W. Olsen <so@bjerk.io>
2021-03-15 16:05:15 +0100

    debug

--- src/main.ts
+++ src/main.ts
@@ -23,55 +23,55 @@ const main = async () => {
 
   debug(`Github Diff: ${JSON.stringify(github, null, 2)}`);
 
-  if (github.created) {
-    const updated = await handleNewIssues(t, github.created);
-    store.github.push(...updated);
-  }
+  try {
+    if (github.created) {
+      const updated = await handleNewIssues(t, github.created);
+      store.github.push(...updated);
+    }
 
-  if (github.updated) {
-    await Promise.all(
-      github.updated.map(async d => {
-        const i = store.github.findIndex(g => g.id === d.id);
-        const s = store.github[i];
-        const itemArgs = await parseTodoistFromGithubIssue(d);
-        if (d.todoistId) {
-          await t.items.update({
-            id: d.todoistId,
-            ...itemArgs,
-          });
-          debug(`GitHub issue #${d.number} was updated.`);
-
-          if (d.state !== s.state) {
-            await handleTodoistUpdatedStatus(t, d, s);
-          }
-
-          store.github[i] = d;
-        }
-      }),
-    );
-  }
-
-  if (github.deleted) {
-    await Promise.all(
-      github.deleted.map(async d => {
-        if (d.todoistId) {
+    if (github.updated) {
+      await Promise.all(
+        github.updated.map(async d => {
           const i = store.github.findIndex(g => g.id === d.id);
-          await t.items.delete({
-            id: d.todoistId,
-          });
-          delete store.github[i];
-        }
-      }),
-    );
+          const s = store.github[i];
+          const itemArgs = await parseTodoistFromGithubIssue(d);
+          if (d.todoistId) {
+            await t.items.update({
+              id: d.todoistId,
+              ...itemArgs,
+            });
+            debug(`GitHub issue #${d.number} was updated.`);
+
+            if (d.state !== s.state) {
+              await handleTodoistUpdatedStatus(t, d, s);
+            }
+
+            store.github[i] = d;
+          }
+        }),
+      );
+    }
+
+    if (github.deleted) {
+      await Promise.all(
+        github.deleted.map(async d => {
+          if (d.todoistId) {
+            const i = store.github.findIndex(g => g.id === d.id);
+            await t.items.delete({
+              id: d.todoistId,
+            });
+            delete store.github[i];
+          }
+        }),
+      );
+    }
+  } catch (e) {
+    debug(`Storing this object: ${JSON.stringify(store, null, 2)}`);
+    await storage.set(store);
+    throw e;
   }
 
-  // await Promise.all(
-  //   t.items.get().map(s => {
-  //     const i = store.github.findIndex(g => g.todoistId === s.id);
-  //     const gh = store.github[i];
-  //   }),
-  // );
-
+  debug(`Storing this object: ${JSON.stringify(store, null, 2)}`);
   await storage.set(store);
 };
 
